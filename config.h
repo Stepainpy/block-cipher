80bea652a21c8da21044d674b1c3036f86037e22
Stepainpy <stepa2014vlg@gmail.com>
2026-02-19 00:05:34 +0300

    upgrade config.h

--- config.h
+++ config.h
@@ -26,14 +26,54 @@ typedef unsigned int  blkcphr_u32_t;
 #endif
 
 #if defined(__GNUC__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wlong-long"
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wlong-long"
 typedef unsigned long long blkcphr_u64_t;
-#pragma GCC diagnostic pop
+#  pragma GCC diagnostic pop
 #elif defined(_MSC_VER)
 typedef unsigned __int64 blkcphr_u64_t;
+#else
+#  error Unsupported copmiler
 #endif
 
 #endif /* __STDC_VERSION__ >= 199901L */
+
+/* Integer byte swap functions */
+
+#if defined(__GNUC__)
+#  define blkcphr_bswap16 __builtin_bswap16
+#  define blkcphr_bswap32 __builtin_bswap32
+#  define blkcphr_bswap64 __builtin_bswap64
+#elif defined(_MSC_VER)
+#  include <stdlib.h>
+#  define blkcphr_bswap16 _byteswap_ushort
+#  define blkcphr_bswap32 _byteswap_ulong
+#  define blkcphr_bswap64 _byteswap_uint64
+#else
+#  error Unsupported compiler
+#endif
+
+/* Detecting endianess */
+
+#if defined(__GNUC__)
+#  if defined(__BYTE_ORDER__)
+#    if   __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#      define BLKCPHR_IS_LITTLE 1
+#      define BLKCPHR_IS_BIG    0
+#    elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#      define BLKCPHR_IS_LITTLE 0
+#      define BLKCPHR_IS_BIG    1
+#    else
+#      error Unknown endianess
+#    endif
+#  else
+#    error Not defined __BYTE_ORDER__
+#  endif
+#elif defined(_MSC_VER)
+#  define BLKCPHR_IS_LITTLE 1
+#  define BLKCPHR_IS_BIG    0
+#else
+#  error Unsupported compiler
+#endif
 
 #endif /* BLOCK_CIPHER_CONFIG_H */
