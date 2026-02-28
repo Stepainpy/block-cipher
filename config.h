0eb647270e7aa54b5281a331215c5fa9eab88f17
Stepainpy <stepa2014vlg@gmail.com>
2026-02-28 12:06:45 +0300

    add compiler macros

--- config.h
+++ config.h
@@ -1,6 +1,21 @@
 #ifndef BLOCK_CIPHER_CONFIG_H
 #define BLOCK_CIPHER_CONFIG_H
 
+/* Compiler detection */
+
+#if defined(__GNUC__)
+#  define BLKCPHR_ON_GNUC 1
+#  if defined(__clang__)
+#    define BLKCPHR_ON_CLANG 1
+#  else
+#    define BLKCPHR_ON_GCC 1
+#  endif
+#elif defined(_MSC_VER)
+#  define BLKCPHR_ON_MSVC 1
+#else
+#  error Unsupported compiler
+#endif
+
 /* Define fixed width integer types */
 
 #if __STDC_VERSION__ >= 199901L
@@ -25,12 +40,12 @@ typedef unsigned long blkcphr_u32_t;
 typedef unsigned int  blkcphr_u32_t;
 #endif
 
-#if defined(__GNUC__)
+#if BLKCPHR_ON_GNUC
 #  pragma GCC diagnostic push
 #  pragma GCC diagnostic ignored "-Wlong-long"
 typedef unsigned long long blkcphr_u64_t;
 #  pragma GCC diagnostic pop
-#elif defined(_MSC_VER)
+#elif BLKCPHR_ON_MSVC
 typedef unsigned __int64 blkcphr_u64_t;
 #else
 #  error Unsupported copmiler
@@ -40,11 +55,11 @@ typedef unsigned __int64 blkcphr_u64_t;
 
 /* Integer byte swap functions */
 
-#if defined(__GNUC__)
+#if BLKCPHR_ON_GNUC
 #  define blkcphr_bswap16 __builtin_bswap16
 #  define blkcphr_bswap32 __builtin_bswap32
 #  define blkcphr_bswap64 __builtin_bswap64
-#elif defined(_MSC_VER)
+#elif BLKCPHR_ON_MSVC
 #  include <stdlib.h>
 #  define blkcphr_bswap16 _byteswap_ushort
 #  define blkcphr_bswap32 _byteswap_ulong
@@ -53,9 +68,9 @@ typedef unsigned __int64 blkcphr_u64_t;
 #  error Unsupported compiler
 #endif
 
-/* Detecting endianess */
+/* Detecting endianness */
 
-#if defined(__GNUC__)
+#if BLKCPHR_ON_GNUC
 #  if defined(__BYTE_ORDER__)
 #    if   __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #      define BLKCPHR_IS_LITTLE 1
@@ -64,12 +79,12 @@ typedef unsigned __int64 blkcphr_u64_t;
 #      define BLKCPHR_IS_LITTLE 0
 #      define BLKCPHR_IS_BIG    1
 #    else
-#      error Unknown endianess
+#      error Unknown endianness
 #    endif
 #  else
 #    error Not defined __BYTE_ORDER__
 #  endif
-#elif defined(_MSC_VER)
+#elif BLKCPHR_ON_MSVC
 #  define BLKCPHR_IS_LITTLE 1
 #  define BLKCPHR_IS_BIG    0
 #else
