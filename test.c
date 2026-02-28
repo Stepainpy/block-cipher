2d782e9e4632ce915467e7f1535b36d932b41583
Stepainpy <stepa2014vlg@gmail.com>
2026-02-28 18:27:55 +0300

    add test for modes

--- test.c
+++ test.c
@@ -11,6 +11,9 @@
 #include "lea/lea.h"
 #include "tea/tea.h"
 
+#include "mgm16/mgm16.h"
+#include "mgm8/mgm8.h"
+
 /* use ANSI coloring */
 #if 1
 #  define OK   "\x1b[32mOK\x1b[0m"
@@ -251,8 +254,220 @@ void cipher_test(void) {
     /* -------------------------------------------------------------------------------- */
 }
 
+typedef struct {
+    unsigned char data[256];
+    size_t count, cursor;
+} buffer_t;
+
+void buffer_init(buffer_t* buf, const void* data, size_t size) {
+    memcpy(buf->data, data, size);
+    buf->count = size;
+    buf->cursor = 0;
+}
+
+size_t buffer_read(void* out, size_t sz, size_t cnt, void* ud) {
+    size_t i, rd = 0; buffer_t* buf = ud;
+    for (i = 0; i < cnt; i++) {
+        if (buf->count - buf->cursor < sz) break;
+        memcpy((char*)out + i * sz, buf->data + buf->cursor, sz);
+        buf->cursor += sz; ++rd;
+    }
+    return rd;
+}
+
+size_t buffer_write(const void* out, size_t sz, size_t cnt, void* ud) {
+    size_t i, wr = 0; buffer_t* buf = ud;
+    for (i = 0; i < cnt; i++) {
+        if (sizeof buf->data - buf->cursor < sz) break;
+        memcpy(buf->data + buf->cursor, (const char*)out + i * sz, sz);
+        buf->count += sz; buf->cursor += sz; ++wr;
+    }
+    return wr;
+}
+
+#define mode_test_case_aead(mname, cname, ksz, vsz, asz, psz, csz, tsz, key, iv, authdat, plain, cipher, tag) \
+do { \
+    unsigned char T[tsz]; buffer_t P, C; int i; \
+    \
+    puts("[=============== Encryption ===============]"); \
+    \
+    printf("- Setup cipher with %3i bits key    ... ", ksz); \
+    puts(mname##_set_encode_func(cname##_block_encode) == 0 \
+        && cname##_init(key, ksz) == 0 ? OK : FAIL); \
+    \
+    printf("- Set initialization vector         ... "); \
+    puts(mname##_set_init_vector(iv) == 0 ? OK : FAIL); \
+    \
+    printf("- Set associated authenticated data ... "); \
+    puts(mname##_set_auth_data(authdat, asz) == 0 ? OK : FAIL); \
+    \
+    buffer_init(&P, plain, psz); memset(&C, 0, sizeof C); \
+    printf("- Set reader (plaintext)            ... "); \
+    puts(mname##_set_reader(buffer_read , &P) == 0 ? OK : FAIL); \
+    printf("- Set writer (ciphertext)           ... "); \
+    puts(mname##_set_writer(buffer_write, &C) == 0 ? OK : FAIL); \
+    \
+    printf("- Encryption process                ... "); \
+    puts(mname##_encryption() == 0 ? OK : FAIL); \
+    \
+    printf("- Check ciphertext                  ... "); \
+    if (memcmp(C.data, cipher, csz) == 0) puts(OK); \
+    else { \
+        puts(FAIL); \
+        printf("  > expected: ");                     \
+        for (i = 0; i < csz; i++)                 \
+            printf("%02x", cipher[i] & 0xFF);         \
+        putchar('\n');                                \
+        printf("  > received: ");                     \
+        for (i = 0; i < csz; i++)                 \
+            printf("%02x", C.data[i]);                     \
+        putchar('\n');                                \
+    } \
+    \
+    printf("- Get authentication tag            ... "); \
+    puts(mname##_get_auth_tag(T) == 0 ? OK : FAIL); \
+    \
+    printf("- Check authentication tag          ... "); \
+    if (memcmp(T, tag, tsz) == 0) puts(OK); \
+    else { \
+        puts(FAIL); \
+        printf("  > expected: ");                     \
+        for (i = 0; i < tsz; i++)                 \
+            printf("%02x", tag[i] & 0xFF);         \
+        putchar('\n');                                \
+        printf("  > received: ");                     \
+        for (i = 0; i < tsz; i++)                 \
+            printf("%02x", T[i]);                     \
+        putchar('\n');                                \
+    } \
+    \
+    puts("[=============== Decryption ===============]"); \
+    \
+    printf("- Setup cipher with %3i bits key    ... ", ksz); \
+    puts(mname##_set_encode_func(cname##_block_encode) == 0 \
+        && cname##_init(key, ksz) == 0 ? OK : FAIL); \
+    \
+    printf("- Set initialization vector         ... "); \
+    puts(mname##_set_init_vector(iv) == 0 ? OK : FAIL); \
+    \
+    printf("- Set associated authenticated data ... "); \
+    puts(mname##_set_auth_data(authdat, asz) == 0 ? OK : FAIL); \
+    \
+    printf("- Set authentication tag            ... "); \
+    puts(mname##_set_auth_tag(T) == 0 ? OK : FAIL); \
+    \
+    memcpy(&P, &C, sizeof C); P.cursor = 0; \
+    memset(&C, 0, sizeof C); \
+    printf("- Set reader (ciphertext)           ... "); \
+    puts(mname##_set_reader(buffer_read , &P) == 0 ? OK : FAIL); \
+    printf("- Set writer (plaintext)            ... "); \
+    puts(mname##_set_writer(buffer_write, &C) == 0 ? OK : FAIL); \
+    \
+    printf("- Decryption process                ... "); \
+    puts(mname##_decryption() == 0 ? OK : FAIL); \
+    \
+    printf("- Check plaintext                   ... "); \
+    if (memcmp(C.data, plain, psz) == 0) puts(OK); \
+    else { \
+        puts(FAIL); \
+        printf("  > expected: ");                     \
+        for (i = 0; i < psz; i++)                 \
+            printf("%02x", plain[i] & 0xFF);         \
+        putchar('\n');                                \
+        printf("  > received: ");                     \
+        for (i = 0; i < psz; i++)                 \
+            printf("%02x", C.data[i]);                     \
+        putchar('\n');                                \
+    } \
+} while (0)
+
+void mode_test(void) {
+    /* -------------------------------------------------------------------------------- */
+
+    puts("Testing Multilinear Galois Mode (MGM), 128 bit:");
+    mode_test_case_aead(
+        mgm16, kuznyechik, 256, 16, 41, 67, 67, 16,
+        /* K */ "\x88\x99\xaa\xbb\xcc\xdd\xee\xff\x00\x11\x22\x33\x44\x55\x66\x77"
+                "\xfe\xdc\xba\x98\x76\x54\x32\x10\x01\x23\x45\x67\x89\xab\xcd\xef",
+        /* V */ "\x11\x22\x33\x44\x55\x66\x77\x00\xff\xee\xdd\xcc\xbb\xaa\x99\x88",
+        /* A */ "\x02\x02\x02\x02\x02\x02\x02\x02\x01\x01\x01\x01\x01\x01\x01\x01"
+                "\x04\x04\x04\x04\x04\x04\x04\x04\x03\x03\x03\x03\x03\x03\x03\x03"
+                "\xea\x05\x05\x05\x05\x05\x05\x05\x05",
+        /* P */ "\x11\x22\x33\x44\x55\x66\x77\x00\xff\xee\xdd\xcc\xbb\xaa\x99\x88"
+                "\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99\xaa\xbb\xcc\xee\xff\x0a"
+                "\x11\x22\x33\x44\x55\x66\x77\x88\x99\xaa\xbb\xcc\xee\xff\x0a\x00"
+                "\x22\x33\x44\x55\x66\x77\x88\x99\xaa\xbb\xcc\xee\xff\x0a\x00\x11"
+                "\xaa\xbb\xcc",
+        /* C */ "\xa9\x75\x7b\x81\x47\x95\x6e\x90\x55\xb8\xa3\x3d\xe8\x9f\x42\xfc"
+                "\x80\x75\xd2\x21\x2b\xf9\xfd\x5b\xd3\xf7\x06\x9a\xad\xc1\x6b\x39"
+                "\x49\x7a\xb1\x59\x15\xa6\xba\x85\x93\x6b\x5d\x0e\xa9\xf6\x85\x1c"
+                "\xc6\x0c\x14\xd4\xd3\xf8\x83\xd0\xab\x94\x42\x06\x95\xc7\x6d\xeb"
+                "\x2c\x75\x52",
+        /* T */ "\xcf\x5d\x65\x6f\x40\xc3\x4f\x5c\x46\xe8\xbb\x0e\x29\xfc\xdb\x4c"
+    );
+    putchar('\n');
+
+    /* -------------------------------------------------------------------------------- */
+
+    puts("Testing Multilinear Galois Mode (MGM), 128 bit:");
+    mode_test_case_aead(
+        mgm16, kuznyechik, 256, 16, 16, 0, 0, 16,
+        /* K */ "\x99\xaa\xbb\xcc\xdd\xee\xff\x00\x11\x22\x33\x44\x55\x66\x77\xfe"
+                "\xdc\xba\x98\x76\x54\x32\x10\x01\x23\x45\x67\x89\xab\xcd\xef\x88",
+        /* V */ "\x11\x22\x33\x44\x55\x66\x77\x00\xff\xee\xdd\xcc\xbb\xaa\x99\x88",
+        /* A */ "\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01",
+        /* P */ "",
+        /* C */ "",
+        /* T */ "\x79\x01\xe9\xea\x20\x85\xcd\x24\x7e\xd2\x49\x69\x5f\x9f\x8a\x85"
+    );
+    putchar('\n');
+
+    /* -------------------------------------------------------------------------------- */
+
+    puts("Testing Multilinear Galois Mode (MGM), 64 bit:");
+    mode_test_case_aead(
+        mgm8, magma, 256, 8, 41, 67, 67, 8,
+        /* K */ "\xff\xee\xdd\xcc\xbb\xaa\x99\x88\x77\x66\x55\x44\x33\x22\x11\x00"
+                "\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff",
+        /* V */ "\x12\xde\xf0\x6b\x3c\x13\x0a\x59",
+        /* A */ "\x01\x01\x01\x01\x01\x01\x01\x01\x02\x02\x02\x02\x02\x02\x02\x02"
+                "\x03\x03\x03\x03\x03\x03\x03\x03\x04\x04\x04\x04\x04\x04\x04\x04"
+                "\x05\x05\x05\x05\x05\x05\x05\x05\xea",
+        /* P */ "\xff\xee\xdd\xcc\xbb\xaa\x99\x88\x11\x22\x33\x44\x55\x66\x77\x00"
+                "\x88\x99\xaa\xbb\xcc\xee\xff\x0a\x00\x11\x22\x33\x44\x55\x66\x77"
+                "\x99\xaa\xbb\xcc\xee\xff\x0a\x00\x11\x22\x33\x44\x55\x66\x77\x88"
+                "\xaa\xbb\xcc\xee\xff\x0a\x00\x11\x22\x33\x44\x55\x66\x77\x88\x99"
+                "\xaa\xbb\xcc",
+        /* C */ "\xc7\x95\x06\x6c\x5f\x9e\xa0\x3b\x85\x11\x33\x42\x45\x91\x85\xae"
+                "\x1f\x2e\x00\xd6\xbf\x2b\x78\x5d\x94\x04\x70\xb8\xbb\x9c\x8e\x7d"
+                "\x9a\x5d\xd3\x73\x1f\x7d\xdc\x70\xec\x27\xcb\x0a\xce\x6f\xa5\x76"
+                "\x70\xf6\x5c\x64\x6a\xbb\x75\xd5\x47\xaa\x37\xc3\xbc\xb5\xc3\x4e"
+                "\x03\xbb\x9c",
+        /* T */ "\xa7\x92\x80\x69\xaa\x10\xfd\x10"
+    );
+    putchar('\n');
+
+    /* -------------------------------------------------------------------------------- */
+
+    puts("Testing Multilinear Galois Mode (MGM), 64 bit:");
+    mode_test_case_aead(
+        mgm8, magma, 256, 8, 0, 8, 8, 8,
+        /* K */ "\x99\xaa\xbb\xcc\xdd\xee\xff\x00\x11\x22\x33\x44\x55\x66\x77\xfe"
+                "\xdc\xba\x98\x76\x54\x32\x10\x01\x23\x45\x67\x89\xab\xcd\xef\x88",
+        /* V */ "\x00\x77\x66\x55\x44\x33\x22\x11",
+        /* A */ "",
+        /* P */ "\x22\x33\x44\x55\x66\x77\x00\xff",
+        /* C */ "\x6a\x95\xe1\x42\x6b\x25\x9d\x4e",
+        /* T */ "\x33\x4e\xe2\x70\x45\x0b\xec\x9e"
+    );
+    putchar('\n');
+
+    /* -------------------------------------------------------------------------------- */
+}
+
 int main(void) {
     putchar('\n');
     cipher_test();
+    mode_test();
     return 0;
 }
